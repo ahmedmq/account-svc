1a3af2fbcdb995c0aa9093bd4b7f168b154a0787
Abhishek Sawhney <sawhneyab@vmware.com>
2022-01-24 14:04:16 +0530

    Add profiles for integration test
    Signed-off-by: Mushtaq Ahmed <amushtaq@vmware.com>

---
+++ src/test/java/com/trainingdemo/accountsvc/controller/DemoIntegrationTest.java
@@ -0,0 +1,17 @@
+package com.trainingdemo.accountsvc.controller;
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("integration-test")
+public class DemoIntegrationTest {
+
+	@Test
+	void sample() {
+
+	}
+}
